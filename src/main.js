63d1e5222a2d46a9554e4b62002745f39c5bd0a2
Greg Walker <mgwalker@gmail.com>
2016-01-31 19:15:38 -0600

    Be a decent citizen and don't try to do everything at once Check each cipher and report at the end Progress bar

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@
 const sslinfo = require("sslinfo");
 const chalk = require("chalk");
 const tlsHandshake = require("./tls/tls-handshake");
+const Queue = require("nbqueue");
 
 const ciphers = require("../ciphers.json");
 const cipherIDs = [ ];
@@ -18,67 +19,61 @@ function getCipherName(id) {
 	}
 }
 
-//process.exit(0);
-//*/
-
 const host = process.argv[2];
+const sslVersions = [
+	"SSLv1.0", "SSLv2.0", "SSLv3.0",
+	"TLSv1.0", "TLSv1.1", "TLSv1.2"
+];
 
-tlsHandshake(host, cipherIDs, "TLSv1.0")
-	.then(msg => {
-		console.log(`Server supports ${msg.tlsVersion} with cipher suite ${getCipherName(msg.cipherSuite)}`);
-	})
-	.catch(e => {
-		console.error("Error:");
-		console.error(e);
+const progress = new (require("progress"))("Testing ciphers [:bar] :current/:total (:percent) :etas", {
+	complete: "=",
+	incomplete: " ",
+	width: 40,
+	total: sslVersions.length * ciphers.length
+});
+
+const tlsChecks = { };
+function tick(next, sslVersion, cipherID, pass) {
+	tlsChecks[sslVersion].push({
+		name: getCipherName(cipherID),
+		supported: pass
 	});
 
-/*
-if (process.argv.length < 3) {
-	console.log();
-	console.log("Usage: ssl-peeker <host> [port]");
-	console.log();
-} else {
-	const hostname = process.argv[2];
-	let port = Number(process.argv[3]) || 443;
-
-	if (port < 0 || port > 65535) {
-		port = 443;
-	}
-
-	console.log("==================================================");
-	console.log(`  Peeking at ${hostname}:${port}`);
-
-	sslinfo.getServerResults({ host: hostname, port: port })
-		.done(function(results) {
-			console.log("--- Certificate ----------------------------------");
-			console.log(`  CN: ${results.cert.subject.commonName}`);
-			console.log(`  ON: ${results.cert.subject.organizationName}`);
-			console.log(`  Issuer:`);
-			console.log(`    CN: ${results.cert.issuer.commonName}`);
-			console.log(`    ON: ${results.cert.issuer.organizationName}`);
-			console.log(`  Valid:`);
-			console.log(`    After: ${results.cert.notBefore}`);
-			console.log(`    Until: ${results.cert.notAfter}`);
-			console.log("--- Protocols ------------------------------------");
-			for (const proto of results.protocols) {
-				console.log(`  ${proto.name} - ${proto.enabled ? chalk.green("ENABLED") : chalk.red(`${proto.error ? chalk.magenta(`unknown - ${proto.error}`) : "disabled"}`)}`);
-			}
-			console.log("--- Ciphers --------------------------------------");
-			for (let cipherMethod in results.ciphers) {
-				if (results.ciphers.hasOwnProperty(cipherMethod)) {
-					const cipherFamily = results.ciphers[cipherMethod];
-
-					console.log(`  ${cipherFamily.name} ciphers:`);
-
-					for (const cipher of cipherFamily.enabled) {
-						console.log(`    ${cipher}`);
-					}
-				}
-			}
-		},
-		function(error) {
-			console.log("Error");
-			console.log(error);
-		});
+	progress.tick(1);
+	setTimeout(next, 300);
 }
-*/
+
+const queue = new (require("nbqueue"))(10);
+const promises = [ ];
+for(let sslVersion of sslVersions) {
+	tlsChecks[sslVersion] = [ ];
+	for(let cipher of ciphers) {
+		queue.add(next => {
+			promises.push(
+				tlsHandshake(host, cipher.id, sslVersion)
+					.then(msg => tick(next, sslVersion, cipher.id, true))
+					.catch(e => tick(next, sslVersion, cipher.id, false))
+			);
+		});
+	}
+}
+
+queue.done(() => {
+	Promise.all(promises).then(() => {
+		console.log("\n");
+		for(let sslVersion of Object.keys(tlsChecks)) {
+			const supportedCiphers = tlsChecks[sslVersion].filter(cipher => cipher.supported);
+			console.log(sslVersion);
+			console.log("--------------------");
+
+			if(supportedCiphers.length > 0) {
+				for(let cipher of supportedCiphers.sort((a, b) => a < b)) {
+					console.log(`   ${chalk.green("✓")} ${cipher.name}`);
+				}
+			} else {
+				console.log(`   ${chalk.red("✗")} no supported ciphers`);
+			}
+			console.log("\n");
+		}
+	}).catch(e => console.log(e));
+});
